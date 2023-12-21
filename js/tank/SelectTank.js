0cda5d978ae0e299476a440cd6395e5189bf928a
余冠豪 <yugh@zhcpa.cn>
2023-12-21 17:35:57 +0800

    SelectTank自己draw，不再继承Spirit

--- js/tank/SelectTank.js
+++ js/tank/SelectTank.js
@@ -1,31 +1,35 @@
 import Tank from './Tank.js'
 import { DIRECTION } from '../const/WORLD.js'
-import { POS } from '../const/IMAGE.js'
+import { POS, RESOURCE_IMAGE } from '../const/IMAGE.js'
+import { step } from '../action/movement.js'
 
 const { UP, DOWN, RIGHT, LEFT } = DIRECTION
 
 // 菜单选择坦克
 export default class SelectTank extends Tank {
   constructor(context, y1 = 250, y2 = 281) {
-    super(context, 'selectTank', 140, y1, 27, y2 - y1, RIGHT)
+    super(context, 'selectTank')
+    this.x = 140
+    this.size = 27
+    this.y = y1
     this.y1 = y1
     this.y2 = y2
-    // 多画几帧，不然看不出来
-    this.deltaYs = [0, 0, 27, 27]
-    this.current = 0
+    this.speed = y2 - y1
+    this.direction = RIGHT
+    this.frames = 0
+    this.slowTimes = 2
+    this.seriesLength = 2
   }
-  get sourcePosition() {
+  draw() {
+    this.frames++
+    const index = Math.floor(this.frames / this.slowTimes) % this.seriesLength
     const [x, y] = POS[this.type]
-    return [x, y + this.deltaY, this.size, this.size]
-  }
-  get deltaY() {
-    this.current = (this.current + 1) % this.deltaYs.length
-    return this.deltaYs[this.current]
+    this.ctx.drawImage(RESOURCE_IMAGE, ...[x, y + this.size * index, this.size, this.size], ...[this.x, this.y, this.size, this.size])
   }
   move(direction) {
-    super.move(direction)
+    const [_, y] = step(direction, this.speed, [this.x, this.y])
     // 只改变y坐标
-    this.y = Math.min(this.y, this.y2)
+    this.y = Math.min(y, this.y2)
     this.y = Math.max(this.y, this.y1)
   }
 }
